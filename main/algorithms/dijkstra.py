7833a8653a60dbde1bfc9ecaaae915044129584b
hlefebvr <henri.pasdecalais@yahoo.fr>
2018-04-12 16:05:04 +0200

    externalize dijkstra and bellman algorithms to module

--- main/algorithms/dijkstra.py
+++ main/algorithms/dijkstra.py
@@ -1,11 +1,8 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 from heapq import *
-import os
 
-BASE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/';
-
-def dijkstra (s, t, voisins):
+def shortest_path (s, t, voisins):
     M = set()
     d = {s: 0}
     p = {}
@@ -30,27 +27,3 @@ def dijkstra (s, t, voisins):
         x = p[x]
         path.insert(0, x)
     return d[t], path
-
-starting_node = "StopPoint:OIF59587"; # Républiques
-end_node = "StopPoint:OIF59659"; # Rivoli louvre
-
-# starting_node = "PI1";
-# end_node = "PI6";
-
-graph = eval ( open(BASE_PATH + '../data_generation/output/graph_pi.json', 'r').read() );
-
-def voisins (s):
-    return [] if s not in graph else graph[s];
-
-time, stop_ids = dijkstra(starting_node, end_node, voisins);
-del graph;
-id2name = eval ( open(BASE_PATH + '../data_generation/output/id2name_pi.json', 'r').read() );
-
-print 'Temps estimé : ', time, ' minutes';
-print '----------------------------'
-
-for id in stop_ids:
-    stop_info = id2name[id];
-    for info in reversed(stop_info):
-        print info.replace('\u00e9', 'é'),'\t',;
-    print
---
+++ main/exec_bellman.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+from algorithms.bellman import shortest_path;
+import os;
+import time;
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/';
+
+graph = eval( open(BASE_PATH + './data_generation/output/graph_pred.json', 'r').read() );
+id2name = eval( open( BASE_PATH + './data_generation/output/id2name.json', 'r').read() );
+
+starting_node = "StopPoint:OIF59587"; # Républiques
+end_node = "StopPoint:OIF59659"; # Rivoli louvre
+
+algo_start_time = time.time();
+distance, stop_ids = shortest_path(graph, starting_node, end_node);
+algo_end_time = time.time();
+
+print 'Temps de calcul : ', (algo_end_time - algo_start_time), ' s';
+print 'Temps estimé : ', distance, ' minutes';
+print '----------------------------'
+for id in stop_ids:
+    stop_info = id2name[id];
+    for info in reversed(stop_info):
+        print info.replace('\u00e9', 'é'),'\t',;
+    print
